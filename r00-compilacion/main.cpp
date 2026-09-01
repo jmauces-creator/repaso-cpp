d4579b0053c5b0fdfe770a094b86e27c1867e6d7
José Manuel <a01613631@tec.mx>
2026-08-31 20:32:50 -0600

    R0: compilación desde la terminal

---
+++ r00-compilacion/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "suma.h"
+
+int main(){
+
+   std::cout << sumar(3,5) << "\n";
+   return 0;
+}
---
+++ r00-compilacion/suma.cpp
@@ -0,0 +1,5 @@
+#include "suma.h"
+
+int sumar(int a, int b){
+    return a + b;
+}
