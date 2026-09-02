a2797bac8ff653f61dd744786f7868c51c260fef
José Manuel <a01613631@tec.mx>
2026-09-01 19:57:16 -0600

    R1 y R2

---
+++ r02-tipos/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout << 7 / 2 << "\n";
+    std::cout << 7 / 2.0 << "\n";
+    std::cout << 7 % 2 << "\n";
+    std::cout << static_cast<int>(3.99) << "\n";
+    std::cout << (1 == 1.0) << "\n";
+    unsigned int u = 3;
+    std::cout << u - 5 << "\n";
+
+}
