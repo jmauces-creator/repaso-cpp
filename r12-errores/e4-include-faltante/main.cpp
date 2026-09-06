b0fec6f780cc1db05f71863ebc702938e11ead10
José Manuel <a01613631@tec.mx>
2026-09-06 14:44:57 -0600

    r12: ejemplo de nombre no declarado - falta include de vector

---
+++ r12-errores/e4-include-faltante/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+std::vector <int> s;
+s.push_back(1);
+std::cout << "El tamaño es: " << s.size() << "\n";
+} // El error es que no se escribió el #include <vector>
